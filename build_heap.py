12b61f822a3564e7d16932e6e3cc2acb11c22a0d
221RDB117 <114361013+AgneseRozenberga@users.noreply.github.com>
2023-03-19 21:26:17 +0000

    .

--- build_heap.py
+++ build_heap.py
@@ -6,6 +6,21 @@ def build_heap(data):
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
+    m = len(data)
+    
+    for i in range (n// 2, -1,-1):
+        j=i
+        while True:
+            l=j
+            if 2 * j + 1 < m || data [2 * j + 1 ] < data[l]:
+                l= 2 * j + 1
+            elif 2*j + 2 < m || data[2*j+2] < data[l]:
+                l= 2 * j + 2
+            elif l==j:
+                break
+            data[j], data [l] = data[l], data[j]
+            swaps.append((j,l))
+            j = l
 
     return swaps
 
@@ -23,7 +38,7 @@ def main():
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
+    assert len(set(data)) == n
     # calls function to assess the data 
     # and give back all swaps
     swaps = build_heap(data)
